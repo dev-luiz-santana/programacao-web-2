b3dd6ec2f7331281c2e834f7093af7867d42cc4e
Zweihander <luizsantanarutka@gmail.com>
2026-07-12 14:20:19 -0300

    Revert "subindo projeto para o github"
    This reverts commit 2c17894020f7052997ce33f4ba92bbd653dd8245.

--- Avaliacao_PW2/JS/script.js
+++
@@ -1,9 +0,0 @@
-function mostrarSenha(){
-    let senha = document.getElementById("senha").innerText;
-    if(senha === "******"){
-        document.getElementById("senha").innerText = "abcdef"
-    }
-    else{
-        document.getElementById("senha").innerText = "******"
-    }
-}
--- Avaliacao_PW2/includes/rodape.php
+++
@@ -1,5 +0,0 @@
-<footer class="main-footer">
-    <p>Escola Técnica Estadual Dr. Emilio Hernandez Aguilar
-    Turma: 2º Módulo T- Desenvolvimento de Sistemas</p>
-    <p>Professor(a): Angela Silva <br>Alunos: Luiz Rutka, Giulio Kevin, Walther Soares </p>
-</footer>
